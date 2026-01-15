d7e207f4ea4033535ce02b834275fa1aeb99d1bd
Aero <Amalgamking@gmail.com>
2026-01-14 20:32:15 -0500

    The Master page worked out. next: create blog posts and work on pages

--- pythonProject2/AmalgampWebsite/pages/urls.py
+++ pythonProject2/AmalgampWebsite/pages/urls.py
@@ -4,7 +4,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('pages/', views.pages, name='pages'),
+    path('', views.pages, name='pages'),
+    path("post/<int:pk>/", views.pages_detail, name="blog_detail"),
+    path("category/<category>/", views.pages_category, name="blog_category"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- pythonProject2/AmalgampWebsite/pages/views.py
+++ pythonProject2/AmalgampWebsite/pages/views.py
@@ -1,6 +1,35 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from .models import Post, Comment
 
+def pages_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "pages/index.html", context)
 def pages(request):
-  template = loader.get_template('master.html')
+  template = loader.get_template('index.html')
   return HttpResponse(template.render())
+
+def pages_category(request, category):
+    posts = Post.objects.filter(
+      categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+      "category": category,
+      "posts": posts,
+    }
+    return render(request, "pages/category.html", context)
+
+
+def pages_detail(request, pk):
+  post = Post.objects.get(pk=pk)
+  comments = Comment.objects.filter(post=post)
+  context = {
+    "post": post,
+    "comments": comments,
+  }
+
+  return render(request, "pages/detail.html", context)
